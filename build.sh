31c468f78bd63c2359d73e608d7393ea186762c2
arsh ahmed <syedarsh7860@gmail.com>
2026-03-26 22:29:39 +0530

    Add build script with NLTK downloads

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+pip install -r requirements.txt
+python3 -c "
+import nltk
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('punkt_tab')
+"
+python3 main.py
+```
+
